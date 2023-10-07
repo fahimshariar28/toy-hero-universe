efe2019053f160432c42782506cd615ec146586d
Fahim Shariar <fahimshariar28@gmail.com>
2023-10-07 18:10:18 +0600

    Collection setup

--- server/index.js
+++ server/index.js
@@ -30,6 +30,11 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+    //   collection
+    const toyHeroCollection = client
+      .db("toy-hero")
+      .collection("toy-hero-collection");
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
