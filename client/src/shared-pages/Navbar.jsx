b8e63d4499d14e035dbb328ff282eb7bbacffed5
Fahim Shariar <fahimshariar28@gmail.com>
2023-10-07 21:41:16 +0600

    Small change in navbar

--- client/src/shared-pages/Navbar.jsx
+++ client/src/shared-pages/Navbar.jsx
@@ -159,15 +159,6 @@ const Navbar = () => {
                 className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
               >
                 <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
-                </li>
-                <li>
-                  <a>Settings</a>
-                </li>
-                <li>
                   <Link onClick={handleLogout} to="/login">
                     Logout
                   </Link>
